55cbc657918b1e1e9ec1e9da2d2b670ae25bd07f
reinhash <reinhard.scheuerle@gmail.com>
2025-03-20 19:45:33 +0100

    Refactor migration handling to use HashMap for migration files and enhance new migration name generation

--- src/rebase.rs
+++ src/rebase.rs
@@ -1,6 +1,9 @@
 use git2::Repository;
 
-use std::path::{Path, PathBuf};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
 fn find_staged_migrations(repo_path: &Path) -> Vec<PathBuf> {
     let mut staged_migrations = Vec::new();
@@ -56,9 +59,35 @@ fn find_staged_migrations(repo_path: &Path) -> Vec<PathBuf> {
     staged_migrations
 }
 
+fn get_number_from_migration(migration: &PathBuf) -> Option<u32> {
+    if let Some(file_name) = migration.file_name() {
+        let file_name_str = file_name.to_string_lossy();
+        if let Some(pos) = file_name_str.find('_') {
+            if pos > 0 && file_name_str[..pos].chars().all(|c| c.is_digit(10)) {
+                let number_str = &file_name_str[..pos];
+                return number_str.parse::<u32>().ok();
+            }
+        }
+    }
+    None
+}
+
+fn get_name_from_migration(migration: &PathBuf) -> Option<String> {
+    if let Some(file_name) = migration.file_name() {
+        let file_name_str = file_name.to_string_lossy();
+        if let Some(pos) = file_name_str.find('_') {
+            if pos > 0 {
+                return Some(file_name_str[pos + 1..].to_string());
+            }
+        }
+    }
+    None
+}
+
 #[derive(Debug)]
 struct MigrationGroup {
-    migration_files: Vec<PathBuf>,
+    migration_files: HashMap<u32, PathBuf>,
+    new_migration_names: Option<HashMap<PathBuf, PathBuf>>,
 }
 
 impl MigrationGroup {
@@ -70,10 +99,13 @@ impl MigrationGroup {
     fn find_last_head_migration(&self) -> Option<PathBuf> {
         let lowest_number = self.lowest_migration_number()?;
         let files = self.sorted_files_in_dir()?;
+
+        let migration_paths = self.migration_paths();
+
         for file in files {
             if let Some(file_name) = file.file_name() {
                 let file_path = file.to_path_buf();
-                if self.migration_files.contains(&file_path) {
+                if migration_paths.contains(&file_path) {
                     continue;
                 }
                 let file_name_str = file_name.to_string_lossy();
@@ -95,8 +127,10 @@ impl MigrationGroup {
 
     fn directory(&self) -> Option<PathBuf> {
         self.migration_files
-            .get(0)
-            .and_then(|path| path.parent().map(|p| p.to_path_buf()))
+            .values()
+            .next()
+            .and_then(|path| path.parent())
+            .map(|p| p.to_path_buf())
     }
 
     /// Finds all current files in the migration directory.
@@ -119,35 +153,56 @@ impl MigrationGroup {
     /// Finds the lowest migration number in the group.
     fn lowest_migration_number(&self) -> Option<u32> {
         let mut lowest_number = None;
-        for migration in &self.migration_files {
-            if let Some(file_name) = migration.file_name() {
-                let file_name_str = file_name.to_string_lossy();
-                if let Some(pos) = file_name_str.find('_') {
-                    if pos > 0 && file_name_str[..pos].chars().all(|c| c.is_digit(10)) {
-                        let number_str = &file_name_str[..pos];
-                        if let Ok(number) = number_str.parse::<u32>() {
-                            lowest_number =
-                                Some(lowest_number.map_or(number, |n: u32| n.min(number)));
-                        }
-                    }
-                }
+        for number in self.migration_files.keys() {
+            if lowest_number.is_none() || *number < lowest_number.unwrap() {
+                lowest_number = Some(*number);
             }
         }
         lowest_number
     }
+
+    fn migration_paths(&self) -> Vec<PathBuf> {
+        self.migration_files.values().cloned().collect::<Vec<_>>()
+    }
+
+    fn generate_new_migration_names(&mut self, last_head_migration: PathBuf) {
+        let last_head_migration_number = get_number_from_migration(&last_head_migration).unwrap();
+        let mut new_migration_names = HashMap::new();
+
+        let mut old_migration_paths = self.migration_paths();
+        old_migration_paths.sort_by(|a, b| a.cmp(b));
+        //  add 1 to last_head_migration_number to get the new migration number
+        let mut new_migration_number = last_head_migration_number + 1;
+        for old_migration_path in old_migration_paths {
+            let old_migration_name = get_name_from_migration(&old_migration_path).unwrap();
+            let new_migration_name = format!("{:04}_{}", new_migration_number, old_migration_name);
+            let new_migration_path = self
+                .directory()
+                .unwrap()
+                .join(new_migration_name)
+                .with_extension("py");
+            new_migration_names.insert(old_migration_path, new_migration_path);
+            new_migration_number += 1;
+        }
+
+        self.new_migration_names = Some(new_migration_names);
+    }
 }
 
 impl MigrationGroup {
-    fn group_by_dir(migrations: Vec<PathBuf>) -> Vec<MigrationGroup> {
+    fn create(migrations: Vec<PathBuf>) -> Vec<MigrationGroup> {
         let mut grouped_migrations: Vec<MigrationGroup> = Vec::new();
 
         for migration in migrations {
             let parent_dir = migration.parent().unwrap();
             let mut found_group = false;
+            let migration_number = get_number_from_migration(&migration);
 
             for group in &mut grouped_migrations {
-                if group.migration_files[0].parent() == Some(parent_dir) {
-                    group.migration_files.push(migration.clone());
+                if group.migration_files.values().next().unwrap().parent() == Some(parent_dir) {
+                    group
+                        .migration_files
+                        .insert(migration_number.unwrap(), migration.clone());
                     found_group = true;
                     break;
                 }
@@ -155,7 +210,8 @@ impl MigrationGroup {
 
             if !found_group {
                 grouped_migrations.push(MigrationGroup {
-                    migration_files: vec![migration],
+                    migration_files: HashMap::from([(migration_number.unwrap(), migration)]),
+                    new_migration_names: None,
                 });
             }
         }
@@ -166,9 +222,11 @@ impl MigrationGroup {
 
 pub fn fix(search_path: &str, dry_run: bool) {
     let migrations = find_staged_migrations(Path::new(search_path));
-    let migration_groups = MigrationGroup::group_by_dir(migrations);
-    for group in migration_groups {
-        let last_head_migration = group.find_last_head_migration();
-        println!("last head migration: {:?}", last_head_migration);
+    let mut migration_groups = MigrationGroup::create(migrations);
+    for group in migration_groups.iter_mut() {
+        let last_head_migration = group.find_last_head_migration().unwrap();
+        println!("Last head migration: {:?}", last_head_migration);
+        group.generate_new_migration_names(last_head_migration);
     }
+    println!("Migration groups: {:?}", migration_groups);
 }
